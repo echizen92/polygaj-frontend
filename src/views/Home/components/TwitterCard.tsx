a13db1bda79743a5a10383747a0d134c8f4cfb85
minibluekirbyfi <faiz.rashid@gmx.us>
2021-07-20 16:37:32 +0800

    twittercard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'polygaj'
+            screenName: 'pbluekatkirbyfi'
           }}
           options={{
             height: '300',
