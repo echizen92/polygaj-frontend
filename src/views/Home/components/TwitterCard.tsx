a2cbe409243dd243162fce6e5598a67d54bc657e
minibluekirbyfi <faiz.rashid@gmx.us>
2021-07-21 01:16:03 +0800

    twittercard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'bluekatkirbyfi'
+            screenName: 'xorFinance'
           }}
           options={{
             height: '300',
